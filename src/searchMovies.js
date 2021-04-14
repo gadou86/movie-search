04d6b004c25acc0a691dbfd2dc2626ca33a0f593
gad dayan <gadou86@gmail.com>
2021-04-14 11:46:57 -0400

    MovieSearchApp

---
+++ src/searchMovies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function SearchMovie() {
+  return(
+    <form className="form">
+      <label htmlFor="query" className="label">Movie Name</label>
+      <input type="text" className="input" name="query"
+        placeholder="i.e Jurrasic Park"
+      />
+      <button className="button" type="submit">Search</button>
+    </form>
+  )
+}
